e1e7aabc3676efc3352c70046c871b8ff2bd3d75
Marek Cermak <macermak@redhat.com>
2019-02-28 12:58:35 +0100

    Allow suppress_warnings to be loaded as extension
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   __init__.py

---
+++ jupyter_tools/__init__.py
@@ -0,0 +1,6 @@
+"""Tools and utilities to improve your experience with Jupyter Notebooks."""
+
+def load_ipython_extension(ipython):
+    """Load the IPython extension."""
+    from .warnings import suppress_warnings  # autoregister
+

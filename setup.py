33ae21c2a6ba1ec950a83219f94d19a6781431db
Marek Cermak <macermak@redhat.com>
2019-05-05 18:05:20 +0200

    Remove python 3.5 version compatibility classifier
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Visualization",
